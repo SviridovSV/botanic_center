aeb0ebec5f630ba77ea691b628a3a29125dc1f06
SviridovSV <sviridov.sv82@gmail.com>
2017-08-03 16:51:24 +0300

    add devise, add user model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "botanic_center_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'botanic_center.herokuapp' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
